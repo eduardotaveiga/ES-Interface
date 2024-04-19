d49444f174bcc8f41fbbbea80badc2896e5f3e14
Eduardo Veiga <etaveiga@gmail.com>
2024-04-19 00:40:01 -0300

    Add files via upload

--- UFABConnect_API.py
+++ UFABConnect_API.py
@@ -28,7 +28,51 @@ def criar_grupo(nome, descricao):
         return 0
 
 
+def fazer_post(titulo, conteudo):
+    with open('token.txt', 'r') as file:
+        bearer_token = file.read()
+        headers = {"Authorization": f"Bearer {bearer_token}"}
+    re = requests.post('http://a352189960f3d40cb8a56a6ae34ef5b6-58486820.us-east-1.elb.amazonaws.com/api/v1/posts',
+                       json={'groupId': 1, 'title': titulo,
+                             'content': conteudo},
+                       headers=headers)
+    if re.status_code == 201:
+        return 1
+    else:
+        return 0
 
 
-#if '__name__' == '__main__':
-    #pass
+def carregar_publicacao(id):
+    with open('token.txt', 'r') as file:
+        bearer_token = file.read()
+        headers = {"Authorization": f"Bearer {bearer_token}"}
+
+    re = requests.get(
+        f'http://a352189960f3d40cb8a56a6ae34ef5b6-58486820.us-east-1.elb.amazonaws.com/api/v1/posts/by-id/{id}',
+        headers=headers)
+
+    try:
+        dados = re.json()
+    except:
+        pass
+    else:
+        return [dados['title'], dados['content'], dados['author']['username'], dados['author']['email']]
+
+
+def carregar_grupos():
+    with open('token.txt', 'r') as file:
+        bearer_token = file.read()
+        headers = {"Authorization": f"Bearer {bearer_token}"}
+
+    re = requests.get('http://a352189960f3d40cb8a56a6ae34ef5b6-58486820.us-east-1.elb.amazonaws.com/api/v1/groups',
+                      headers=headers)
+    saida = []
+    for c in re.json():
+        saida.append(c['name'])
+
+    return saida
+
+
+
+if '__name__' == '__main__':
+    pass
--- pages/homepage.py
+++ pages/homepage.py
@@ -1,6 +1,24 @@
 
 import streamlit as st
-from PIL import Image, ImageDraw, ImageFont
+from UFABConnect_API import fazer_post, carregar_publicacao, carregar_grupos
+
+# Logica para manter o botão de fazer postagem como True de forma 'permanente"
+if 'button' not in st.session_state:
+    st.session_state.button = False
+
+def click_button(i, titulo, conteudo):
+    if i == 0:
+        st.session_state.button = not st.session_state.button
+    else:
+        pass
+        #resp = fazer_post(titulo, conteudo)
+        #st.session_state.button = not st.session_state.button
+
+if 'button_visao_grupo' not in st.session_state:
+    st.session_state.button_visao_grupo = False
+
+def visao_grupo_button():
+    st.session_state.button_visao_grupo = not st.session_state.button_visao_grupo
 
 # Layout de colunas para organizar os componentes
 
@@ -11,12 +29,11 @@ coluna_postagens = st
 
 imagem_url = 'https://i.ibb.co/XjtdCtD/Whats-App-Image-2024-03-07-at-17-50-22.png'
 st.sidebar.image(imagem_url, width=200, caption='')
-
-pagina_home = coluna_paginas.button('Home')
+pagina_home = coluna_paginas.button('Home', on_click=visao_grupo_button)
 
 # Ações do usuário (execução das ações fica lá em baixo)
 
-fazer_postagem = st.sidebar.button('Novo post')
+fazer_postagem = st.sidebar.button('Novo post', on_click=click_button, args=[0, '', ''])
 st.sidebar.write('')
 st.sidebar.write('')
 
@@ -37,61 +54,94 @@ st.sidebar.write('')
 # Grupos já existentes
 
 st.sidebar.write("Grupos")
-grupos = ["Grupo 1", "Grupo 2", "Grupo 3"]
-for i in range(0, len(grupos)):
-    grupo = coluna_paginas.button(str(grupos[i]))
+grupos = carregar_grupos()
+for gp in grupos:
+    grupo = coluna_paginas.button(gp, on_click=visao_grupo_button)
 
 # Feed
+if st.session_state.button:
+    titulo = st.text_input('Título')
+    conteudo = st.text_area('Descrição')
+    st.file_uploader('Arquivos')
+    voltar = st.button('Voltar', on_click=click_button, args=[0, '', ''])
+    if st.button('Publicar'):
+        resp = fazer_post(titulo, conteudo)
+        if resp == 1:
+            st.write('Publição feita!')
+elif st.session_state.button_visao_grupo:
+    # Prototipo para envio das mensagens
 
-st.text_input('Procurar grupos')
-st.header('Feed')
+    msg_outro_usuario, msg_usuario_atual = st.columns([1, 1])
+    # Seção de exibição das mensagens
+    with msg_outro_usuario:
+        st.markdown(
+            "<div style='background-color: #486a84; padding: 3px; border-radius: 1px; font-size: small;'>Usuário 1</div>",
+            unsafe_allow_html=True)
+        st.markdown(
+            "<div style='background-color: #486a84; padding: 8px; border-radius: 1px;'>Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto</div>",
+            unsafe_allow_html=True)
+        for i in range(8):
+            st.text('')
+        st.markdown(
+            "<div style='background-color: #486a84; padding: 3px; border-radius: 1px; font-size: small;'>Usuário 1</div>",
+            unsafe_allow_html=True)
+        st.markdown(
+            "<div style='background-color: #486a84; padding: 8px; border-radius: 1px;'>Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto</div>",
+            unsafe_allow_html=True)
 
-# Ações do usuário
+    with msg_usuario_atual:
+        for i in range(7):
+            st.text('')
+        st.markdown(
+            "<div style='background-color: #000080; padding: 3px; border-radius: 1px; font-size: small;'>Você</div>",
+            unsafe_allow_html=True)
+        st.markdown(
+            "<div style='background-color: #000080; padding: 8px; border-radius: 1px;'>Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto</div>",
+            unsafe_allow_html=True)
 
-todos_grupos = ["Grupo 1", "Grupo 2", "Grupo 3", "Grupo 4", "Grupo 5", "Grupo 6"]
+    for i in range(4):
+        st.text('')
+    input_placeholder = st.empty()
+    message = input_placeholder.text_input("Digite sua mensagem...", "")
 
-#if fazer_postagem:
-#    nova_postagem = st.text_area('Faça uma nova postagem:')
-#    enviar_postagem = st.button('Enviar')
-#
-#    if enviar_postagem:
-#        st.write('Postagem enviada:', nova_postagem)
-#        st.write(nova_postagem)
-#        feed_postagens.append(nova_postagem)
+    if st.button("Enviar") and message:
+        with msg_usuario_atual:
+            for i in range(8):
+                st.text('')
+            st.markdown(
+                "<div style='background-color: #000080; padding: 3px; border-radius: 1px; font-size: small;'>Você</div>",
+                unsafe_allow_html=True)
+            st.markdown(f"<div style='background-color: #000080; padding: 8px; border-radius: 1px;'>{message}</div>",
+                        unsafe_allow_html=True)
+else:
+    st.text_input('Procurar grupos')
+    st.header('Feed')
+
+    # Ações do usuário
+
+    # Função para formatar e exibir um post
+    def exibir_post(titulo, postagem, nome_usuario, email, url_foto_perfil):
+        with st.form(key=chave_formatacao):
+            st.image(url_foto_perfil, width=75, caption='')
+            st.write(f"""{nome_usuario} ({email})""")
+            st.header(f'{titulo}')
+            st.write(postagem)
+            like = st.form_submit_button("Curti")
+
+    # Incluir lógica para carregar grupos
+    def exibir_grupo(nome_grupo, url_foto_grupo, cod_acesso):
+        with st.form():
+            st.image(url_foto_grupo, width=75, caption='')
+            st.write(f"""{nome_grupo}""")
+            entrar = st.button("Entrar")
 
 
-# Função para formatar e exibir um post
+    # Exibir todos os posts, formatados
+    url_foto_perfil_exemplo = imagem_url
 
-def exibir_post(postagem, nome_usuario, url_foto_perfil):
-    with st.form(key=chave_formatacao):
-        st.image(url_foto_perfil, width=75, caption='')
-        st.write(f"""{nome_usuario}""")
-        st.write(postagem)
-        like = st.form_submit_button("Curti")
-
-
-def exibir_grupo(nome_grupo, url_foto_grupo, cod_acesso):
-    with st.form():
-        st.image(url_foto_grupo, width=75, caption='')
-        st.write(f"""{nome_grupo}""")
-        entrar = st.button("Entrar")
-
-
-# Exibir todos os posts, formatados
-
-postagem_exemplo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-nome_usuario_exemplo = "Usuário Exemplo"
-url_foto_perfil_exemplo = imagem_url
-
-dados_posts = [[postagem_exemplo, nome_usuario_exemplo, url_foto_perfil_exemplo],
-               [postagem_exemplo, nome_usuario_exemplo, url_foto_perfil_exemplo],
-               [postagem_exemplo, nome_usuario_exemplo, url_foto_perfil_exemplo]
-               ]
-
-iteracao = 0
-chave_formatacao = "my_form_" + str(iteracao)
-
-for i in dados_posts:
-    exibir_post(i[0], i[1], i[2])
-    iteracao = iteracao + 1
-    chave_formatacao = "my_form_" + str(iteracao)
+    chave_formatacao = "my_form_" + str(0)
+    for i in range(17, 24):  # Carregar alguns exemplos, logica para carregar mais recentes depois
+        dp = carregar_publicacao(i)
+        if dp is not None:
+            exibir_post(dp[0], dp[1], dp[2], dp[3], url_foto_perfil_exemplo)
+            chave_formatacao = "my_form_" + str(i)
